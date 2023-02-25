7e523dc191e4f7137228f74ca2106ff123659fa9
cg37 <cg37@outlook.com>
2023-02-25 23:42:49 +0800

    1

--- day25第七章 回溯算法/17_电话号码的字母组合.cpp
+++ day25第七章 回溯算法/17_电话号码的字母组合.cpp
@@ -1,13 +1,42 @@
 #include "head.h"
 class Solution {
 private:
+    const string letterMap[10] = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz",
+    };
     vector<string> res;
     string s;
-    void backtracking(const string& digits, int index) {
-        
+    void backTracking(const string& digits, int index) {
+        if (index == digits.size()) {
+            res.push_back(s);
+            return;
+        }
+        int digit = digits[index] - '0';
+        string letters = letterMap[digit];
+
+        for (int i = 0; i < letters.size(); i++) {
+            s.push_back(letters[i]);
+            backTracking(digits, index + 1);
+            s.pop_back();
+        }
     }
 public:
     vector<string> letterCombinations(string digits) {
-
+        s.clear();
+        res.clear();
+        if (digits.size() == 0) {
+            return res;
+        }
+        backTracking(digits, 0);
+        return res;
     }
 };
