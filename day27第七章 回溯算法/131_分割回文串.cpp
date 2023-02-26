d5e39895b421b64ff69940a2c468b9c163025f29
cg37 <cg37@outlook.com>
2023-02-26 23:29:00 +0800

    1

--- day27第七章 回溯算法/131_分割回文串.cpp
+++ day27第七章 回溯算法/131_分割回文串.cpp
@@ -1,8 +1,38 @@
 #include "head.h"
 
 class Solution {
+private: 
+    vector<string> path;
+    vector<vector<string> > res;
+    bool isPalindrome(const string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            if(s[i] != s[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void backTracking(const string& s, int startIndex) {
+        if (startIndex >= s.size()) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < s.size(); i++) {
+            if (isPalindrome(s, startIndex, i)) {
+                string str = s.substr(startIndex, i - startIndex + 1);
+                path.push_back(str);
+            } else {
+                continue;
+            }
+            backTracking(s, i+1);
+            path.pop_back();
+        }
+    }
 public:
     vector<vector<string>> partition(string s) {
-
+        res.clear();
+        path.clear();
+        backTracking(s, 0);
+        return res;
     }
 };
